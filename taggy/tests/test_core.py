c81fbc03b81f2364a4624cdd23d54afd24edb95c
Michael Karotsieris <michael.karotsieris@gmail.com>
2020-01-26 04:24:54 +0000

    .

--- taggy/tests/test_core.py
+++ taggy/tests/test_core.py
@@ -4,25 +4,22 @@ from .context import taggy
 
 
 class TaggyCoreTests(unittest.TestCase):
-    
-    def __init__(self):
-        super().__init__()
-        self.tag_engine = taggy.TagEngine()
-    
+    def setUp(self):
+        self.taggy = taggy.Taggy()
 
 
 class TestAddText(TaggyCoreTests):
     
     def test_raises_not_implemented(self):
         with self.assertRaises(NotImplementedError):
-            self.tag_engine.add_text(text='')
+            self.taggy.add_text(text='')
 
 
 class TestGetTags(TaggyCoreTests):
 
     def test_raises_not_implemented(self):
         with self.assertRaises(NotImplementedError):
-            self.tag_engine.get_tags(document_ids=[])
+            self.taggy.get_tags(document_ids=[])
 
 
 if __name__ == "__main__":
